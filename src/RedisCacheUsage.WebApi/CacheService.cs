a8846d14e748cd1440e4da7f4840c58575bfbebb
meggmumbi <margaretmumbi16@gmail.com>
2023-06-08 14:59:51 +0300

    Redis Cache

--- src/RedisCacheUsage.WebApi/CacheService.cs
+++ src/RedisCacheUsage.WebApi/CacheService.cs
@@ -1,4 +1,5 @@
-﻿using RedisCacheUsage.Application.Interfaces;
+﻿using Newtonsoft.Json;
+using RedisCacheUsage.Application.Interfaces;
 using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
@@ -6,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RedisCacheUsage.Infrastracture.Service
+namespace RedisCacheUsage.WebApi
 {
     public class CacheService : ICacheService
     {
@@ -24,17 +25,29 @@ namespace RedisCacheUsage.Infrastracture.Service
 
         public T GetData<T>(string key)
         {
-            throw new NotImplementedException();
+            var value = _db.StringGet(key);
+            if(!string.IsNullOrEmpty(value))
+            {
+                return JsonConvert.DeserializeObject<T>(value);
+            }
+            return default;
         }
 
         public object RemoveData(string Key)
         {
-            throw new NotImplementedException();
+            bool _isKeyExist = _db.KeyExists(Key);
+            if(_isKeyExist==true)
+            {
+                return _db.KeyDelete(Key);
+            }
+            return false;
         }
 
         public bool SetData<T>(string Key, T value, DateTimeOffset expirationTime)
         {
-            throw new NotImplementedException();
+            TimeSpan expiryTime = expirationTime.DateTime.Subtract(DateTime.Now);
+            var isSet = _db.StringSet(Key,JsonConvert.SerializeObject(value),expiryTime);
+            return isSet;
         }
     }
 
